93d054948fbac4f270c1d9a8efabd30fa8449b63
Timofei Larkin <lllamnyp@gmail.com>
2023-12-01 00:41:39 +0300

    draft up client and token maintenance routines

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,9 +1,80 @@
 package client
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/lllamnyp/oidc/internal/token"
 )
 
-type Client interface {
-	RequestToken() io.Reader
+type confidentialClient struct {
+	clientID      string
+	clientSecret  string
+	tokenEndpoint string
+}
+
+func (c *confidentialClient) Token() token.Token {
+	vals := url.Values{}
+	vals.Add("client_id", c.clientID)
+	vals.Add("client_secret", c.clientSecret)
+	vals.Add("grant_type", "client_credentials")
+	req, err := http.NewRequest(
+		http.MethodPost,
+		c.tokenEndpoint,
+		strings.NewReader(vals.Encode()),
+	)
+	if err != nil {
+		return token.Token{}
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return token.Token{}
+	}
+	defer resp.Body.Close()
+	dec := json.NewDecoder(resp.Body)
+	tok := token.Token{}
+	err = dec.Decode(&tok)
+	if err != nil {
+		return token.Token{}
+	}
+	return tok
+}
+
+func NewConfidentialClient(clientID, clientSecret, issuerURL string) *confidentialClient {
+	endpoint, err := getTokenEndpoint(issuerURL)
+	if err != nil {
+		return nil
+	}
+	return &confidentialClient{clientID, clientSecret, endpoint}
+}
+
+func getTokenEndpoint(issuerURL string) (string, error) {
+	resp, err := http.Get(issuerURL + "/.well-known/openid-configuration")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	v := make(map[string]interface{})
+	err = json.Unmarshal(b, &v)
+	if err != nil {
+		return "", err
+	}
+	endpointIface, ok := v["token_endpoint"]
+	if !ok {
+		return "", fmt.Errorf("could not get token endpoint")
+	}
+	endpointStr, ok := endpointIface.(string)
+	if !ok {
+		return "", fmt.Errorf("could not get token endpoint")
+	}
+	return endpointStr, nil
 }
--- oidc.go
+++ oidc.go
@@ -24,15 +24,41 @@ func (t *threadSafeTokenSource) Token() token.Token {
 }
 
 func (t *threadSafeTokenSource) updateToken() {
+	// Executing TokenSource.Token() might take a while so we get a copy first
+	// and only then acquire the lock to write the new value to the struct.
+	updatedToken := t.TokenSource.Token()
 	t.Lock()
 	defer t.Unlock()
-	t.t = t.TokenSource.Token()
+	t.t = updatedToken
 }
 
 type maintainedTokenSource struct {
 	threadSafeTokenSource
 	d time.Time
+	e time.Duration
+	f uint64
 }
 
-func (t *maintainedTokenSource) maintain() {
+func (t *maintainedTokenSource) updateDeadline() time.Time {
+	if t.f < 2 {
+		t.f = 2
+	}
+	return t.d.Add(-t.e / time.Duration(t.f))
+}
+
+func (t *maintainedTokenSource) updateToken() {
+	if t.updateDeadline().Before(time.Now()) {
+		t.threadSafeTokenSource.updateToken()
+		t.e = time.Duration(t.t.ExpiresIn) * time.Second
+		t.d = time.Now().Add(t.e)
+	}
+}
+
+func (t *maintainedTokenSource) maintainToken() {
+	for {
+		if updateDeadline := t.updateDeadline(); updateDeadline.Before(time.Now()) {
+			time.Sleep(time.Until(updateDeadline))
+		}
+		t.updateToken()
+	}
 }
